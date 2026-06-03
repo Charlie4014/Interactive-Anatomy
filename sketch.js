e94a0da4c97a2c2dfa6eff550cef0d5fbbd73ef2
Charlie4014 <charlie@docrobinschool.com>
2026-06-03 10:10:59 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@
 //
 
 const version = "pre5";
-const devMode = true;
-const pageID = "axial_skeleton1"
+const devMode = false;
+const pageID = "axial_skeleton1";
 
 let img;
 let data;
@@ -11,11 +11,11 @@ let button;
 let text_display;
 let text_display_cooldown;
 let render_scale = 0.25;
-const text_display_max_cooldown = 300;
+var text_display_max_cooldown = 300;
 
 function preload() {
   data = loadJSON("data.json", () => {
-    data = data[pageID]
+    data = data[pageID];
     img = loadImage(data.image.imageSource);
   });
 }
@@ -31,7 +31,9 @@ function draw() {
   background(220);
   render_image(render_scale);
 
-  text_display_cooldown -= 1;
+  
+  // hide text display when cool down reaches 0
+  text_display_cooldown -= 1;    
   if (text_display_cooldown == 0) {
     text_display.hide();
     text_display.visible = false;
@@ -46,10 +48,10 @@ function create_points() {
   let p;
   for (let i = 0; i < data.points.length; i++) {
     p = data.points[i];
-    button.push(new_point());
+    button.push(new_point(p));
   }
 
-  function new_point() {
+  function new_point(p) {
     let button = createButton("");
     button.position(
       p.posX * render_scale - p.radius / 2,
@@ -58,9 +60,9 @@ function create_points() {
     button.style("border-radius", "50%");
     button.size(p.radius, p.radius);
     button.style("background-color", "rgb(31,106,247)");
- //   button.style("color", "rgb(253,193,84)");
+    //   button.style("color", "rgb(253,193,84)");
     button.style("border", "2px solid #2704FF");
-    
+
     button.mousePressed(() => {
       button_clicked(p);
     });
@@ -69,6 +71,7 @@ function create_points() {
 }
 
 function button_clicked(info) {
+  text_display_max_cooldown = round(info. delay)
   text_display_cooldown = text_display_max_cooldown;
   select("#text_title").html(info.title);
   select("#text_body").html(info.description);
@@ -78,6 +81,7 @@ function button_clicked(info) {
 
 function mousePressed() {
   if (text_display_cooldown < text_display_max_cooldown) {
+    
     text_display.hide();
     text_display.visible = false;
     text_display_cooldown = 1;
@@ -95,10 +99,18 @@ function setup_text_displays() {
 `);
 
   text_display.hide();
+
   text_display.style("width", width * 0.95 + "px");
   text_display.position(width * 0.015, 0);
   text_display.style("padding", "14px 0");
-  text_display.style("background", "linear-gradient(#6df56d, #2f8f2f)");
+
+  text_display.style(
+    "background",
+    "linear-gradient(rgba(25,180,25,0.7), rgba(15,120,15,0.7))"
+  );
+  text_display.style("backdrop-filter", "blur(2px)");
+  text_display.style("-webkit-backdrop-filter", "blur(2px)");
+
   text_display.style("color", "white");
   text_display.style("text-align", "center");
   text_display.style("border-radius", "18px");
@@ -107,6 +119,13 @@ function setup_text_displays() {
     "box-shadow",
     "inset 0px 4px 8px rgba(255,255,255,0.35), 0px 8px 16px rgba(0,0,0,0.45)"
   );
+
   text_display.style("text-shadow", "0px 2px 3px rgba(0,0,0,0.6)");
 }
 
+function mousePressed() {
+  if (devMode) {
+    console. log("mouseX: "+(mouseX/render_scale))
+    console. log("mouseY: "+(mouseY/render_scale))
+  }
+}
